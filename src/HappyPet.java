d907f396f9510a2255e9f60a7996ee743a587f1a
league <league@wts8.attlocal.net>
2018-04-15 17:28:47 -0700

    happy Pet, PIE

--- src/HappyPet.java
+++ src/HappyPet.java
@@ -1,29 +1,42 @@
 import javax.swing.JOptionPane;
 
 public class HappyPet {
-	
-	// 2. Add the following variable to the next line: static int happinessLevel = 0;
+
+	// 2. Add the following variable to the next line: static int happinessLevel =
+	// 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel = 0;
+
 	public static void main(String[] args) {
-		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
-		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
+		// 1. Ask the user what kind of pet they want to buy, and store their answer in
+		// a variable
+		int pet = JOptionPane.showOptionDialog(null, "what pet do you wanna buy?", "Pet options", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Dragon", "Horse", "Dog", "Cat" }, null);
+		String petName = "";
+		if (pet==0) {
+			petName="Dragon";
+		}
 		
-			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
-			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
-			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 
-			// 5. Use user input to call the appropriate method created in step 4.
+		// 3. Use showOptionDialog to ask the user what they want to do to make their
+		// pet happy
+		// (eg: cuddle, food, water, take a walk, groom, clean up poop).
+		// Make sure to customize the title and question too.
+	//	if (pet == 0) {
+	
+		int task = JOptionPane.showOptionDialog(null, "What do you want to do with your " + petName, "Pet activity", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "cuddle", "feed", "walk" }, null);
+		//}
+		// 5. Use user input to call the appropriate method created in step 4.
 
-			// 6. If you determine the happiness level is large enough, tell the
-			//    user that he loves his pet and use break; to exit for loop.
+		// 6. If you determine the happiness level is large enough, tell the
+		// user that he loves his pet and use break; to exit for loop.
 
 	}
 
 	// 4. Create methods to handle each of your user selections.
-	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
-	//    and INCREMENT the pet's happiness Level.
+	// Each method should create a pop-up with the pet's response (eg. cat might
+	// purr when pet),
+	// and INCREMENT the pet's happiness Level.
 }
--- src/PiAloud.java
+++ src/PiAloud.java
@@ -3,15 +3,39 @@
 
 
 import java.io.IOException;
+import java.net.StandardSocketOptions;
 import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+import org.omg.Messaging.SyncScopeHelper;
 
 public class PiAloud {
 
 	// 1. Make a main method and make sure your program can run
-
+public static void main(String[] args) {
+	String pie = "3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148";
+	//String user = JOptionPane.showInputDialog("hwat is dur nex numbar");
+	//3int num = Integer.parseInt(user);
+	
+	for (int i=0;i<107;i++) {
+		char C = getInputFromUser();
+		if (C == pie.charAt(i)) {
+			System.out.println("Correct");
+		}
+		else {
+			System.out.println("INCORRECT");
+		}
+	System.out.println(pie.charAt(i));
+	speak(pie.charAt(i));
+	
+	
+	}
+}
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
 
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
+
 
 	// 4. Print ALL the digits of of Pi (hint: use a loop)
 
